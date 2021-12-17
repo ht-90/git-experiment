bf59c024cf11da798a5845e714e8df6951b2e7f1
ht-90 <hayato.taka75@gmail.com>
2021-12-17 11:26:00 +1000

    added initial workflow and sphinx docs

---
+++ docs/source/conf.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'Test Documentation'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = []
+
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,4 @@
+Test Documentation
+==================
+
+Test documentation for Github Pages
